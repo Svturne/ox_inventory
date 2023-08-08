d2fffd60e1380592f15aeb4b2be41447ced1f9a0
svturne <mehdi.bad@outlook.com>
2023-08-08 19:24:22 +0200

    server items

--- modules/items/server.lua
+++ modules/items/server.lua
@@ -10,15 +10,15 @@ Items.containers = require 'modules.items.containers'
 
 -- Possible metadata when creating garbage
 local trash = {
-	{description = 'An old rolled up newspaper.', weight = 200, image = 'trash_newspaper'},
-	{description = 'A discarded burger shot carton.', weight = 50, image = 'trash_burgershot'},
-	{description = 'An empty soda can.', weight = 20, image = 'trash_can'},
-	{description = 'A mouldy piece of bread.', weight = 70, image = 'trash_bread'},
-	{description = 'An empty ciggarette carton.', weight = 10, image = 'trash_fags'},
-	{description = 'A slightly used pair of panties.', weight = 20, image = 'panties'},
-	{description = 'An empty coffee cup.', weight = 20, image = 'trash_coffee'},
-	{description = 'A crumpled up piece of paper.', weight = 5, image = 'trash_paper'},
-	{description = 'An empty chips bag.', weight = 5, image = 'trash_chips'},
+	{description = 'Un vieux journal enroulé.', weight = 200, image = 'trash_newspaper'},
+	{description = 'Une boite de burger jeté.', weight = 50, image = 'trash_burgershot'},
+	{description = 'Une canette de soda vide.', weight = 20, image = 'trash_can'},
+	{description = 'Un morceau de pain moisi.', weight = 70, image = 'trash_bread'},
+	{description = 'Une cartouche de cigarettes vide.', weight = 10, image = 'trash_fags'},
+	{description = 'Une culotte légèrement usée.', weight = 20, image = 'panties'},
+	{description = 'Un gobelet de café vide.', weight = 20, image = 'trash_coffee'},
+	{description = 'Un morceau de papier froissé.', weight = 5, image = 'trash_paper'},
+	{description = 'Un sac de chips vide.', weight = 5, image = 'trash_chips'},
 }
 
 ---@param _ table?
