9c6a32e566ff014b20dd49a9dec985f28cf480af
svturne <mehdi.bad@outlook.com>
2023-08-08 19:11:36 +0200

    added stashes

--- data/stashes.lua
+++ data/stashes.lua
@@ -1,17 +1,17 @@
 return {
 	{
-		coords = vec3(452.3, -991.4, 30.7),
+		coords = vec3(362.46539306641, -1595.2403564453, 25.451725006104),
 		target = {
-			loc = vec3(451.25, -994.28, 30.69),
-			length = 1.2,
-			width = 5.6,
-			heading = 0,
-			minZ = 29.49,
-			maxZ = 32.09,
-			label = 'Open personal locker'
+			loc = vec3(362.46539306641, -1595.2403564453, 30.451725006104),
+			length = 0.6,
+			width = 1.8,
+			heading = 340,
+			minZ = 43.34,
+			maxZ = 44.74,
+			label = 'Casier personnel'
 		},
 		name = 'policelocker',
-		label = 'Personal locker',
+		label = 'Casier personnel',
 		owner = true,
 		slots = 70,
 		weight = 70000,
@@ -27,10 +27,10 @@ return {
 			heading = 340,
 			minZ = 43.34,
 			maxZ = 44.74,
-			label = 'Open personal locker'
+			label = 'Casier personnel'
 		},
 		name = 'emslocker',
-		label = 'Personal Locker',
+		label = 'Casier personnel',
 		owner = true,
 		slots = 70,
 		weight = 70000,
