fba7a4db92a12910ba61e1610cb20b6e37840f86
svturne <mehdi.bad@outlook.com>
2023-08-08 19:46:11 +0200

    fix mouse detect

--- modules/utils/client.lua
+++ modules/utils/client.lua
@@ -131,6 +131,47 @@ function Utils.CreateBlip(settings, coords)
 	return blip
 end
 
+local function detectClosestPedToMouse()
+    local plyPed = PlayerPedId()
+    closestPed = nil
+    while isSelectingActive do
+        Citizen.Wait(200)
+        local plyPos = GetEntityCoords(plyPed) 
+        local xClosestDistance = 5.0
+        local yClosestDistance = 5.0
+        for k, currentPed in pairs(GetGamePool('CPed')) do
+            if IsPedAPlayer(currentPed) and currentPed ~= plyPed then
+                local currentPedCoords = GetPedBoneCoords(currentPed, 24818, 0.0, 0.0, 0.0)
+                local distance = GetDistanceBetweenCoords(currentPedCoords, plyPos, true)
+                if distance < 6 then
+                    local _, screenX, screenY = GetScreenCoordFromWorldCoord(currentPedCoords.x, currentPedCoords.y, currentPedCoords.z)
+                    if screenX > 0 and screenY > 0 then
+                        local mouseX, mouseY = GetNuiCursorPosition()
+                        local screenWidth, screenHeight = GetActiveScreenResolution()
+                        if (mouseX <= screenWidth and mouseY <= screenHeight) then
+                            mouseX = mouseX/screenWidth
+                            mouseY = mouseY/screenHeight
+                            local xScreenDistance = math.abs(mouseX-screenX)
+                            local yScreenDistance = math.abs(mouseY-screenY)
+                            if xScreenDistance < 0.03 and yScreenDistance < 0.1 then
+                                if xClosestDistance > xScreenDistance and yClosestDistance > yScreenDistance then
+                                    xClosestDistance = xScreenDistance
+                                    yClosestDistance = yScreenDistance
+                                    closestPed = currentPed
+                                end
+                            else
+                                if currentPed == closestPed then
+                                    closestPed = nil
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
 function Utils.getMousePlayer(cb)
     if (isSelectingActive) then return end
     isSelectingActive = true
