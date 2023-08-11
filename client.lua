dee79d06907991c7948deaac794f576ee32e993a
svturne <mehdi.bad@outlook.com>
2023-08-11 11:48:43 +0200

    change progressBar to circle

--- client.lua
+++ client.lua
@@ -345,9 +345,9 @@ lib.callback.register('ox_inventory:usingItem', function(data)
 			item.disable.combat = true
 		end
 
-		local success = (not item.usetime or lib.progressBar({
+		local success = (not item.usetime or lib.progressCircle({
 			duration = item.usetime,
-			label = item.label or locale('using', data.metadata.label or data.label),
+			label = "",
 			useWhileDead = item.useWhileDead,
 			canCancel = item.cancel,
 			disable = item.disable,
